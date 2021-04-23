ed9798f22a95b868794d997b2ebc7efa9e92e482
andr12f4 <75079112+andr12f4@users.noreply.github.com>
2021-04-23 12:48:15 +0200

    user Models and interface done

--- Zealand Carpool/Interfaces/IUser.cs
+++ Zealand Carpool/Interfaces/IUser.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Zealand_Carpool.Models;
 
 namespace Zealand_Carpool.Interfaces
 {
-    interface IBruger
+    public interface IBruger
     {
+        bool AddUser(User user);
+        User GetUser(Guid id);
+        User GetUser(string email, string password);
+        bool DeleteUser(Guid id);
+        bool UpdateUser(Guid id, User user);
+
+
     }
 }
