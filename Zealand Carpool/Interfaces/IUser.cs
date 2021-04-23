95ec28daa354ba881d70870d9825dc86d1c3027c
andr12f4 <75079112+andr12f4@users.noreply.github.com>
2021-04-23 13:00:16 +0200

    Interface done

--- Zealand Carpool/Interfaces/IUser.cs
+++ Zealand Carpool/Interfaces/IUser.cs
@@ -7,5 +7,10 @@ namespace Zealand_Carpool.Interfaces
 {
     interface IUser
     {
+        bool AddUser(User user);
+        User GetUser(Guid id);
+        User GetUser(string email, string password);
+        bool DeleteUser(Guid id);
+        bool UpdateUser(Guid id, User user);
     }
 }
