da2678b8179e3b6bf8bf220ece9e04827f48aec6
andr12f4 <75079112+andr12f4@users.noreply.github.com>
2021-04-23 12:37:26 +0200

    User models + interface

---
+++ Zealand Carpool/Models/Addresse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zealand_Carpool.Models
+{
+    /// <summary>
+    /// A Model class of Addresses
+    /// With a ToString method that returns it all
+    /// made by Andreas
+    /// </summary>
+    public class Addresse
+    {
+        [Required, StringLength(255), MinLength(1)]
+        public string StreetName { get; set; }
+        [Required, MinLength(1),MaxLength(255)]
+        public int StreetNumber { get; set; }
+        [Required, MinLength(3),MaxLength(4)]
+        public int Postalcode { get; set; }
+        [Required, StringLength(30)]
+        public string CityName { get; set; }
+
+        public Addresse() { }
+        public Addresse(string newStreetName, int newStreetNumber, int newPostalcode, string newCityName)
+        {
+            StreetName = newStreetName;
+            StreetNumber = newStreetNumber;
+            Postalcode = newPostalcode;
+            CityName = newCityName;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Postal code: {Postalcode}, City name: {CityName}, Street name: {StreetName}, Street Number: {StreetNumber}";
+        }
+
+        
+    }
+}
