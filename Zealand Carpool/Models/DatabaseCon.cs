067dc85dcc27550d9267e55c2e39eedeca2dd7aa
andr12f4 <75079112+andr12f4@users.noreply.github.com>
2021-04-26 10:06:34 +0200

    Database connection model

---
+++ Zealand Carpool/Models/DatabaseCon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zealand_Carpool.Models
+{
+    public class DatabaseCon
+    {
+        string ConnString = "Data Source=andreas-zealand-server-dk.database.windows.net;Initial Catalog=Andreas-database;User ID=Andreas;Password=SecretPassword!;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+        
+        public SqlConnection GetConnection()
+        {
+            using (SqlConnection conn = new SqlConnection(ConnString))
+            {
+                conn.Open();
+                return conn;
+            }
+        }   
+
+    }
+}
--- Zealand Carpool/Services/UserDatabase.cs
+++ Zealand Carpool/Services/UserDatabase.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Zealand_Carpool.Interfaces;
+using Zealand_Carpool.Models;
 
 namespace Zealand_Carpool.Services
 {
-    public class UserDatabase
+    public class UserDatabase : IUser
     {
+        public bool AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteUser(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateUser(Guid id, User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
