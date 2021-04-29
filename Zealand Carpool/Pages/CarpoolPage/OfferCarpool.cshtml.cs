a4440fe5d3a2ced039f7f73e0cdd698d91fc185c
Malte <70883688+MalteZea@users.noreply.github.com>
2021-04-29 12:54:02 +0200

    OfferCarpool view

--- Zealand Carpool/Pages/CarpoolPage/OfferCarpool.cshtml.cs
+++ Zealand Carpool/Pages/CarpoolPage/OfferCarpool.cshtml.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Zealand_Carpool.Models;
 
 namespace Zealand_Carpool.Pages.CarpoolPage
 {
     public class OfferCarpoolModel : PageModel
     {
+        private Carpool carpool = new Carpool();
+
+
         public void OnGet()
         {
         }
