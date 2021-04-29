ffb1ed761caea96c8b25c65fd1b841c2001ae87e
Malte <70883688+MalteZea@users.noreply.github.com>
2021-04-29 09:56:38 +0200

    Revert "Merge branch 'master' into Malte"
    This reverts commit a63ff6a7fd6d34bf4dfdb7c09db349cbf99f3843, reversing
    changes made to e21e682219e551aaa733cd52d06e821a83fc8f33.

--- Zealand Carpool/Pages/LoginPage/Logout.cshtml.cs
+++ Zealand Carpool/Pages/LoginPage/Logout.cshtml.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,19 +9,8 @@ namespace Zealand_Carpool.Pages.LoginPage
 {
     public class LogoutModel : PageModel
     {
-        public IActionResult OnGet()
+        public void OnGet()
         {
-            if (User.Identity.IsAuthenticated)
-            { return Page(); }
-            return RedirectToPage("/Index");
-        }
-
-        public IActionResult OnPostLogout()
-        {
-            
-            HttpContext.SignOutAsync();
-            
-            return RedirectToPage("/index");
         }
     }
 }
