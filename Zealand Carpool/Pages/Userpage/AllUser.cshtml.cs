5babb5950ac63c014f576ddb22da951d88597e69
Malte <70883688+MalteZea@users.noreply.github.com>
2021-05-06 13:21:18 +0200

    Edit

--- Zealand Carpool/Pages/Userpage/AllUser.cshtml.cs
+++ Zealand Carpool/Pages/Userpage/AllUser.cshtml.cs
@@ -1,16 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Zealand_Carpool.Interfaces;
+using Zealand_Carpool.Models;
 
 namespace Zealand_Carpool.Pages.Userpage
 {
     public class AllUserModel : PageModel
     {
-        public void OnGet()
+        public List<User> ListOfUsers { get; set; }
+        public User LoggedInUser { get; set; }
+        IUser userInterface;
+        public AllUserModel(IUser iuser)
         {
+            userInterface = iuser;
+        }
+        public IActionResult OnGet(string name)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                List<System.Security.Claims.Claim> listofClaims = User.Claims.ToList();
+                LoggedInUser = userInterface.GetUser(Guid.Parse(listofClaims[0].Value)).Result;
+                ListOfUsers = userInterface.SearchUsers(name);
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("/Index");
+            }
+
+        }
+
+        public IActionResult OnPost()
+        {
+
+            return RedirectToPage("AllUser");
         }
     }
 }
