34e0b9893d9894ffb671cf7150be467828e5d792
AlexTriumf <TriumfAlex@gmail.com>
2021-05-10 14:18:03 +0200

    Revert "Revert "merge""
    This reverts commit 8a9be270160b3736d272542783639815ec1a9a17.

---
+++ Zealand Carpool/Pages/Shared/ProtectedPageRouting.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Zealand_Carpool.Pages.Shared
+{
+    public abstract class ProtectedPageRouting : PageModel
+    {
+        public IActionResult OnGet(string id)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToPage("/Index");
+            }
+            return GetRequest(id);
+        }
+        protected abstract IActionResult GetRequest(string id);
+    }
+}
